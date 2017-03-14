88fedb0853b58ae926d392c5589a9c7f8b8131c8
Richard <pkuanjie@gmail.com>
2017-03-14 23:52:45 +0800

    finish W and A compute

--- CUDA_C/math_image/admm_hw1/admm.cu
+++ CUDA_C/math_image/admm_hw1/admm.cu
@@ -13,7 +13,7 @@ int main(){
     const Size KERNEL_SIZE(15, 15);
     const double SIGMA = 1.5;
     const int SCALE = 100;
-    const double LAMBDA = 1;
+    const double LAMBDA = sqrt(2) - 1;
 
     // image resize, Gaussian blur and noise adding of original image
 
@@ -21,7 +21,7 @@ int main(){
     Mat img_gray_tmp;
     Mat_<double> img_gray;
     cvtColor(img, img_gray_tmp, CV_RGB2GRAY);
-    img_gray_tmp.convertTo(img_gray, CV_64F, 1.0/255.0);
+    img_gray_tmp.convertTo(img_gray, CV_64FC1, 1.0/255.0);
     img.release();
     img_gray_tmp.release();
     resize(img_gray, img_gray, Size(), 1.0 / 4, 1.0 / 4, INTER_LINEAR);
@@ -34,7 +34,7 @@ int main(){
     //waitKey(3000);
     double* value_max_img = new double[1];
     cuda::minMax(img_gray, NULL, value_max_img);
-    Mat_<double> img_noise(img_gray.rows, img_gray.cols, CV_64F);
+    Mat_<double> img_noise(img_gray.rows, img_gray.cols, CV_64FC1);
     randn(img_noise, 0, value_max_img[0]);
     cuda::multiply(img_noise, value_max_img[0] / SCALE, img_noise);
     cuda::add(img_gray, img_noise, img_gray);
@@ -46,11 +46,11 @@ int main(){
 
     // generate matrix A and W
 
-    Mat_<double> A = Mat::eye(img_gray.rows * img_gray.cols, img_gray.rows * img_gray.cols, CV_64F);
+    Mat_<double> A = Mat::zeros(img_gray.rows * img_gray.cols, img_gray.rows * img_gray.cols, CV_64FC1);
     if (img_gray.isContinuous() == 0){
         throw "memory of gray image is not continuous, cannot use cuda!";
     }
-    Mat_<double> W = Mat::eye(img_gray.rows * img_gray.cols, img_gray.rows * img_gray.cols, CV_64F);
+    Mat_<double> W = Mat::zeros(img_gray.rows * img_gray.cols, img_gray.rows * img_gray.cols, CV_64FC1);
     if (img_gray.isContinuous() == 0){
         throw "memory of gray image is not continuous, cannot use cuda!";
     }
@@ -62,28 +62,17 @@ int main(){
     double* W_device;
     cudaMalloc((void**)&W_device, sizeof(double) * W.cols * W.rows);
     cudaMemcpy(W_device, W_host, sizeof(double) * W.cols * W.rows, cudaMemcpyHostToDevice);
-    Mat_<double> Gauss = getGaussianKernel(15, SIGMA, CV_64F);
+    Mat_<double> Gauss = Mat::zeros(15, 15, CV_64FC1);
+    Gauss.at<double>(7, 7) = 1;
+    GaussianBlur(Gauss, Gauss, KERNEL_SIZE, SIGMA, BORDER_WRAP);
     double* Gauss_device;
     cudaMalloc((void**)&Gauss_device, sizeof(double) * Gauss.cols * Gauss.rows);
     cudaMemcpy(Gauss_device, Gauss.ptr<double>(0), sizeof(double) * Gauss.cols * Gauss.rows, cudaMemcpyHostToDevice);
-    dim3 grid(A.rows, A.cols);
     dim3 thread_perblock(Gauss.rows, Gauss.cols);
-    //compute_A<<<grid, thread_perblock>>>(A_device, Gauss_device, A.rows, A.cols, img_gray.rows, img_gray.cols, Gauss.rows, Gauss.cols);
-    compute_W<<<grid, 1>>>(W_device, W.rows, W.cols, img_gray.rows, img_gray.cols, LAMBDA);
+    compute_A<<<A.rows, thread_perblock>>>(A_device, Gauss_device, A.rows, A.cols, img_gray.rows, img_gray.cols, Gauss.rows, Gauss.cols);
+    compute_W<<<W.rows, 1>>>(W_device, W.rows, W.cols, img_gray.rows, img_gray.cols, LAMBDA);
     cudaMemcpy(A_host, A_device, sizeof(double) * A.cols * A.rows, cudaMemcpyDeviceToHost);
     cudaMemcpy(W_host, W_device, sizeof(double) * W.cols * W.rows, cudaMemcpyDeviceToHost);
-    cout << W_host[0] << endl;
-    cout << W_host[1] << endl;
-    cout << W_host[2] << endl;
-    cout << W_host[3] << endl;
-    cout << W_host[4] << endl;
-    cout << W_host[5] << endl;
-    cout << W_host[6] << endl;
-    cout << W_host[7] << endl;
-    cout << W_host[8] << endl;
-    cout << W_host[9] << endl;
-    cout << W_host[10] << endl;
-    cout << W_host[11] << endl;
     cudaFree(Gauss_device);
     cudaFree(A_device);
     cudaFree(W_device);
--- CUDA_C/math_image/admm_hw1/admm_tools.cu
+++ CUDA_C/math_image/admm_hw1/admm_tools.cu
@@ -4,75 +4,74 @@
 
 __global__ void compute_A(double* A_device, double* Gauss_device, int rows, int cols, int ori_rows, int ori_cols, int Gauss_rows, int Gauss_cols)
 {
-    int i = blockIdx.y;
     int j = blockIdx.x;
     int k1 = threadIdx.y;
     int k2 = threadIdx.x;
     int len_x = (Gauss_rows - 1) / 2;
     int len_y = (Gauss_cols - 1) / 2;
-    if (i < rows && j < cols)
+    if (j < cols)
     {
-        int a = j / ori_cols;
-        int b = j % ori_cols;
+        int b = j / ori_rows;
+        int a = j % ori_rows;
         int a1 = a - len_x;
         int b1 = b - len_y;
         int k_a = k1 + a1;
         int k_b = k2 + b1;
         if (k_a >= 0 && k_a < ori_rows && k_b >= 0 && k_b < ori_cols && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[(k_a * ori_cols + k_b) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[(k_b * ori_rows + k_a) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// inner situation
         if (k_a < 0 && k_b >= 0 && k_b < ori_cols && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[((ori_rows + k_a) * ori_cols + k_b) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[((ori_rows + k_a) + k_b * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// top edge
         if (k_a >= ori_rows && k_b >= 0 && k_b < ori_cols && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[((k_a - ori_rows) * ori_cols + k_b) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[((k_a - ori_rows) + k_b * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// bottom edge
         if (k_a >= 0 && k_a < ori_rows && k_b < 0 && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[(k_a * ori_cols + (k_b + ori_cols)) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[(k_a + (k_b + ori_cols) * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// laft edge
         if (k_a >= 0 && k_a < ori_rows && k_b >= ori_cols && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[(k_a * ori_cols + (k_b - ori_cols)) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[(k_a + (k_b - ori_cols) * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// right edge
         if (k_a < 0 && k_b < 0 && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[((k_a + ori_rows) * ori_cols + (k_b + ori_cols)) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[((k_a + ori_rows) + (k_b + ori_cols) * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// left top corner
         if (k_a < 0 && k_b >= ori_cols && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[((k_a + ori_rows) * ori_cols + (k_b - ori_cols)) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[((k_a + ori_rows) + (k_b - ori_cols) * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// right top corner
         if (k_a >= ori_rows && k_b < 0 && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[((k_a - ori_rows) * ori_cols + (k_b + ori_cols)) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[((k_a - ori_rows) + (k_b + ori_cols) * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// left bottom corner
         if (k_a >= ori_rows && k_b >= ori_cols && k1 < Gauss_rows && k2 < Gauss_cols)
         {
-          A_device[((k_a - ori_rows) * ori_cols + (k_b - ori_cols)) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
+          A_device[((k_a - ori_rows) + (k_b - ori_cols) * ori_rows) * cols + j] = Gauss_device[k1 * Gauss_cols + k2];
         }// right bottom corner
+        __syncthreads();
     }
 }
 
 
 __global__ void compute_W(double* W_device, int rows, int cols, int ori_rows, int ori_cols, double LAMBDA)
 {
-    int i = blockIdx.y;
     int j = blockIdx.x;
-    int a = i / ori_cols;
-    int b = i % ori_cols;
-    if (a > 0 && a < ori_rows - 1 && b > 0 && b < ori_cols - 1)
+    int a = j / ori_rows;
+    int b = j % ori_rows;
+    if (a > 0 && a < ori_cols - 1 && b > 0 && b < ori_rows - 1)
     {
-        W_device[((a - 1) * ori_cols + b) * cols + j] = LAMBDA / 2.0;
-        W_device[((a + 1) * ori_cols + b) * cols + j] = LAMBDA / 2.0;
-        W_device[(a * ori_cols + (b - 1)) * cols + j] = LAMBDA / 2.0;
-        W_device[(a * ori_cols + (b + 1)) * cols + j] = LAMBDA / 2.0;
-        W_device[((a - 1) * ori_cols + (b - 1)) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
-        W_device[((a - 1) * ori_cols + (b + 1)) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
-        W_device[((a + 1) * ori_cols + (b - 1)) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
-        W_device[((a + 1) * ori_cols + (b + 1)) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
+        W_device[(j - 1) * cols + j] = LAMBDA / 2.0;
+        W_device[(j + 1) * cols + j] = LAMBDA / 2.0;
+        W_device[(j - ori_rows) * cols + j] = LAMBDA / 2.0;
+        W_device[(j + ori_rows) * cols + j] = LAMBDA / 2.0;
+        W_device[(j - ori_rows - 1) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
+        W_device[(j - ori_rows + 1) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
+        W_device[(j + ori_rows - 1) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
+        W_device[(j + ori_rows + 1) * cols + j] = (1 - LAMBDA) * sqrt(2.0) / 4.0;
     }
 }
