d827dfb611a41d55c4fdb9e8bc33e9c465b9d3b8
Richard <pkuanjie@gmail.com>
2017-03-15 14:34:42 +0800

    iteration finished a half

--- CUDA_C/math_image/admm_hw1/admm.cu
+++ CUDA_C/math_image/admm_hw1/admm.cu
@@ -26,6 +26,8 @@ int main(){
     img.release();
     img_gray_tmp.release();
     resize(img_gray, img_gray, Size(), 1.0 / 4, 1.0 / 4, INTER_LINEAR);
+    Mat_<double> img_ori;
+    img_gray.copyTo(img_ori);
     //namedWindow("img1", WINDOW_NORMAL);
     //imshow("img1", img_gray);
     //waitKey(0);
@@ -48,15 +50,15 @@ int main(){
     // generate matrix A and W
 
     Mat_<double> A = Mat::zeros(img_gray.rows * img_gray.cols, img_gray.rows * img_gray.cols, CV_64FC1);
-    if (img_gray.isContinuous() == 0){
-        throw "memory of gray image is not continuous, cannot use cuda!";
-    }
     Mat_<double> W = Mat::zeros(img_gray.rows * img_gray.cols, img_gray.rows * img_gray.cols, CV_64FC1);
-    if (img_gray.isContinuous() == 0){
-        throw "memory of gray image is not continuous, cannot use cuda!";
-    }
     cudev::GpuMat_<double> A_device(A);
     cudev::GpuMat_<double> W_device(W);
+    if (A_device.isContinuous() == 0){
+        throw "memory of gray image is not continuous, cannot use cuda!";
+    }
+    if (W_device.isContinuous() == 0){
+        throw "memory of gray image is not continuous, cannot use cuda!";
+    }
     Mat_<double> Gauss = Mat::zeros(15, 15, CV_64FC1);
     Gauss.at<double>(7, 7) = 1;
     GaussianBlur(Gauss, Gauss, KERNEL_SIZE, SIGMA, BORDER_WRAP);
@@ -64,10 +66,69 @@ int main(){
     dim3 thread_perblock(Gauss.rows, Gauss.cols);
     compute_A<<<A.rows, thread_perblock>>>(A_device.ptr<double>(0), Gauss_device.ptr<double>(0), A.rows, A.cols, img_gray.rows, img_gray.cols, Gauss.rows, Gauss.cols);
     compute_W<<<W.rows, 1>>>(W_device.ptr<double>(0), W.rows, W.cols, img_gray.rows, img_gray.cols, LAMBDA);
-    A_device.download(A);
-    W_device.download(W);
-    A_device.release();
-    W_device.release();
     Gauss.release();
     Gauss_device.release();
+
+    // Split Bregman Algorithm iteration
+
+    const double mu = 0.5;
+    const double tol = 1e-6;
+    const double delta = 0.5;
+    const int MAX_ITERATION = 1000;
+    Mat_<double> img_gray_t;
+    transpose(img_gray, img_gray_t);
+    Mat_<double> img_gray_t2 = img_gray_t.reshape(0, img_gray.rows * img_gray.cols);
+    cudev::GpuMat_<double> f(img_gray_t2);
+    cudev::GpuMat_<double> f_modified(img_gray_t2);
+    img_gray_t.release();
+    img_gray_t2.release();
+    Mat_<double> source = Mat::zeros(f.rows, f.cols, CV_64FC1);
+    cudev::GpuMat_<double> d(source);
+    cudev::GpuMat_<double> b(source);
+    cudev::GpuMat_<double> u(source);
+    cudev::GpuMat_<double> tmp1(source);
+    cudev::GpuMat_<double> tmp2(source);
+    cudev::GpuMat_<double> tmp3(source);
+    source.release();
+    cuda::gemm(A_device, f, 1.0, 0, 1.0, f, GEMM_1_T);
+    cuda::gemm(A_device, A_device, 1.0, 0, 1.0, A_device, GEMM_1_T);
+    cuda::gemm(W_device, W_device, mu, A_device, 1.0, A_device, GEMM_1_T);
+    A_device.download(A);
+    invert(A, A, DECOMP_CHOLESKY);
+    A_device.upload(A);
+    cuda::gemm(A_device, f, 1.0, 0, 1.0, f, 0);
+    cuda::gemm(A_device, W_device, 1.0, 0, mu, A_device, GEMM_2_T);
+
+
+    for (int i = 0; i < MAX_ITERATION; i++)
+    {
+        cuda::addWeighted(d, 1.0, b, -1.0, 1.0, tmp1);
+        cuda::gemm(A_device, tmp1, 1.0, f, 1.0, u);
+        cuda::gemm(W_device, u, 1.0, b, 1.0, tmp2);
+    }
+
+
+
+
+
+
+
+
+    // result show
+
+    Mat_<double> result;
+    Mat_<double> result_tmp;
+    d.download(result_tmp);
+    result = result_tmp.reshape(0, img_gray.rows);
+    result_tmp.release();
+    transpose(result, result);
+    namedWindow("result show", WINDOW_NORMAL);
+    imshow("result show", result);
+    waitKey(0);
+
+
+    // release memory
+
+    A_device.release();
+    W_device.release();
 }
