2cd6bf12762d44aa9818e6d2444170f01af8096e
Jason An <pkuanjie@gmail.com>
2017-03-15 15:36:18 +0800

    finish coding, need debug

--- CUDA_C/math_image/admm_hw1/admm.cu
+++ CUDA_C/math_image/admm_hw1/admm.cu
@@ -75,6 +75,8 @@ int main(){
     const double tol = 1e-6;
     const double delta = 0.5;
     const int MAX_ITERATION = 1000;
+    const double L = 1;
+
     Mat_<double> img_gray_t;
     transpose(img_gray, img_gray_t);
     Mat_<double> img_gray_t2 = img_gray_t.reshape(0, img_gray.rows * img_gray.cols);
@@ -89,6 +91,7 @@ int main(){
     cudev::GpuMat_<double> tmp1(source);
     cudev::GpuMat_<double> tmp2(source);
     cudev::GpuMat_<double> tmp3(source);
+    cudev::GpuMat_<double> tmp4(source);
     source.release();
     cuda::gemm(A_device, f, 1.0, 0, 1.0, f, GEMM_1_T);
     cuda::gemm(A_device, A_device, 1.0, 0, 1.0, A_device, GEMM_1_T);
@@ -105,30 +108,50 @@ int main(){
         cuda::addWeighted(d, 1.0, b, -1.0, 1.0, tmp1);
         cuda::gemm(A_device, tmp1, 1.0, f, 1.0, u);
         cuda::gemm(W_device, u, 1.0, b, 1.0, tmp2);
+        double tmp_norm = cuda::norm(tmp2, NORM_L2);
+        if (tmp_norm - L / mu > 0)
+        {
+            cuda::addWeighted(tmp2, (tmp_norm - L / mu) / tmp_norm, tmp4, 1.0, 1.0, d);
+        }
+        else
+        {
+            tmp4.copyTo(d);
+        }
+        cuda::gemm(W_device, u, 1.0, d, -1.0, tmp3);
+        cuda::addWeighted(tmp3, delta, b, 1.0, 1.0, b);
+        double r1 = cuda::norm(tmp3, NORM_L2);
+        double r2 = cuda::norm(f_modified, NORM_L2);
+        if (r1 / r2 < tol)
+        {
+            break;
+        }
     }
-
-
-
-
-
-
-
 
     // result show
 
     Mat_<double> result;
     Mat_<double> result_tmp;
-    d.download(result_tmp);
+    u.download(result_tmp);
     result = result_tmp.reshape(0, img_gray.rows);
     result_tmp.release();
     transpose(result, result);
     namedWindow("result show", WINDOW_NORMAL);
     imshow("result show", result);
     waitKey(0);
-
-
+    double error = norm(u, img_ori, NORM_L2);
+    cout << "error of processing: " << error << endl;
     // release memory
 
     A_device.release();
     W_device.release();
+    d.release();
+    b.release();
+    u.release();
+    tmp1.release();
+    tmp2.release();
+    tmp3.release();
+    tmp4.release();
+    img_gray.release();
+    img_ori.release();
+    result.release();
 }
