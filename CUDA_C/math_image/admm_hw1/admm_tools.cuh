b0a0d5dfeaf9a2accaa2e0cbf99c00e67c520c2f
Jason An <pkuanjie@gmail.com>
2017-03-17 13:45:28 +0800

    delete a bug code unfinished because I have use MATLAB finished my work

--- CUDA_C/math_image/admm_hw1/admm_tools.cuh
+++
@@ -1,7 +0,0 @@
-#ifndef ADMM_TOOLS_CUH
-#define ADMM_TOOLS_CUH
-
-__global__ void compute_A(float*, float*, int, int, int, int, int, int);
-__global__ void compute_W(float*, int, int, int, int, float);
-
-#endif
