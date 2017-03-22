ce2158b171bc0d5ca5392d48eb26736e7561ed35
Jason An <pkuanjie@gmail.com>
2017-03-22 18:28:32 +0800

    deep learning hw1 code finished

--- python/deep_learning/hw1/MLP_mnist.py
+++ python/deep_learning/hw1/MLP_mnist.py
@@ -1,21 +1,42 @@
+# this script is to try a mnist digit number classification with MLP by using
+# tensorflow
+# coder: Jie An
+# version: 20170322
+# bug_submission: pkuanjie@gmail.com
+# ==============================================================================
 import numpy as np
 import tensorflow as tf
-import input_data
+import mnist
 
 
-# set up
+# set up HIDDEN_NEURON_NUM = 500
 HIDDEN_NEURON_NUM = 500
 
 # data input and preprocessing
-mnist_data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist_data = mnist.read_data_sets("MNIST_data/", one_hot=True)
 train_x, train_y, test_x, test_y = mnist_data.train.images, mnist_data.train \
     .labels, mnist_data.test.images, mnist_data.test.labels
 
 # weight init
-W_h = tf.Variable(tf.random_normal([768, HIDDEN_NEURON_NUM], stddev=0.01))
-W_o = tf.Variable(tf.random.normal([HIDDEN_NEURON_NUM, 10], stddev=0.01))
+W_h = tf.Variable(tf.random_normal([784, HIDDEN_NEURON_NUM], stddev=0.01))
+W_o = tf.Variable(tf.random_normal([HIDDEN_NEURON_NUM, 10], stddev=0.01))
 
 # construct model
-X = tf.placeholder('float', [None, 768])
-Y = tf.placeholder('float', [None, 10])
-model_mlp = tf.matmul(tf.nn.relu(tf.matmul(X, W_h)), W_o)
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+Y_pre = tf.matmul(tf.nn.relu(tf.matmul(X, W_h)), W_o)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Y_pre, \
+    labels=Y))
+train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
+predict_op = tf.argmax(Y_pre, 1)
+
+# run graph
+with tf.Session() as sess:
+    tf.initialize_all_variables().run()
+    for i in range(100):
+        for start, end in zip(range(0, len(train_x), 100), range(100, \
+            len(train_x) + 1, 100)):
+            sess.run(train_op, feed_dict={X: train_x[start: end], Y: \
+                train_y[start: end]})
+        print('train num = ', i, 'accuracy = ', np.mean(np.argmax(test_y, \
+            axis=1) == sess.run(predict_op, feed_dict={X: test_x, Y: test_y})))
