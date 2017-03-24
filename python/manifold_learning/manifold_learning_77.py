5db18d84f69983267ca3fc4cf093d48ae4ec4887
Richard <pkuanjie@gmail.com>
2017-03-24 23:01:08 +0800

    so sad, code of hw5 maybe wrong

--- python/manifold_learning/manifold_learning_77.py
+++ python/manifold_learning/manifold_learning_77.py
@@ -1,4 +1,4 @@
-# this script is to compute and show result with different distance difinition of CMDS algorithm
+# this script is to compute and show result with different distance definition of CMDS algorithm
 # coder: Jie An
 # version: 20170324
 # bug_submission: pkuanjie@gmail.com
@@ -6,6 +6,51 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy.io as io
 
+# setup
+DIM = 2
+
+# distance compute function
+def distance_compute(img, i, j, method='Eculidean'):
+    """ this function is to compute distance between two data with different methods"""
+    if method == 'Euclidean':
+        return np.linalg.norm(img[i] - img[j])
+    if method == 'lp':
+        return np.sum(np.abs(img[i] - img[j]))
+    if method == 'Angular':
+        return np.linalg.norm((img[i] / np.linalg.norm(img[i])) - (img[j] / np.linalg.norm(img[j])))
+    if method == 'Cosine':
+        return 1 - np.abs(np.dot(np.transpose(img[i]), img[j]) / (np.linalg.norm(img[i]) * np.linalg.norm(img[j])))[0][0]
+
+# import data from mat file and setup
 data = io.loadmat('umist_cropped.mat')['facedat'][0]
-print(data[:, :, 0])
-# plt.imshow(data[0])
+img = []
+for i in range(len(data)):
+    for j in range(np.size(data[i], axis=2)):
+        img.append(np.reshape(data[i][:, :, j], [1, np.size(data[i], axis=0) * np.size(data[i], axis=1)]))
+img = img * 1.0 / 255
+
+# compute distance matrix
+print('start computing distance matrix')
+D = np.zeros([len(img), len(img)])
+for i in range(len(img)):
+    for j in range(i + 1, len(img)):
+       D[i, j] = distance_compute(img, i, j, method='lp')
+       D[j, i] = D[i, j]
+
+# compute Y
+print('start computing projection matrix')
+S = D * D
+S_c = np.zeros([np.size(S, axis=0), np.size(S, axis=1)])
+for i in range(np.size(D, axis=0)):
+    for j in range(np.size(D, axis=1)):
+        S_c[i, j] = S[i, j] - np.mean(S[:, j]) - np.mean(S[i, :]) + np.mean(S)
+G_c = -1 / 2 * S_c
+w, v = np.linalg.eigh(G_c)
+U = v[:, np.size(v, axis=1) - DIM: np.size(v, axis=1)]
+Lambda = np.diag(w[len(w) - DIM: len(w)])
+Y = np.dot(np.sqrt(Lambda), np.transpose(U))
+
+# plot points in 2D
+f1 = plt.figure(1)
+plt.scatter(Y[1, :], Y[0, :])
+plt.show()
---
+++ python/manifold_learning/manifold_learning_93.py
@@ -0,0 +1,63 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.io as sio
+
+# setup
+DIM = 6
+THRESHOLD = 12000
+
+# import data
+print('start import data from mat file')
+data = sio.loadmat('umist_cropped.mat')['facedat'][0]
+LENGTH = 0
+for i in range(len(data)):
+	LENGTH += np.size(data[i], axis=2)
+img = np.zeros([np.size(data[0], axis=0) * np.size(data[0], axis=1), LENGTH])
+count = 0
+for i in range(len(data)):
+    for j in range(np.size(data[i], axis=2)):
+        img[:, count] = (np.reshape(data[i][:, :, j], [np.size(data[i], axis=0) * np.size(data[i], axis=1)]))
+        count += 1
+img = img * 1.0 / 255
+
+# compute average face and centeralize
+print('start computing average face and centeralize')
+x = np.mean(img, axis=1)
+img_c = np.zeros(np.shape(img))
+for i in range(np.size(img, axis=1)):
+	img_c[:, i] = img[:, i] - x
+
+# generate the random matrix
+print('start generate random matrix')
+R = np.random.normal(0, 1, [DIM, np.size(img, axis=0)])
+P = 1 / np.sqrt(DIM) * R
+Y = np.dot(P, img_c)
+
+# recognition faces
+label = np.zeros(LENGTH, dtype=int) 
+count = 0
+for i in range(len(data)):
+	for j in range(np.size(data[i], axis=2)):
+		img_test = np.reshape(data[i][:, :, j], [np.size(img, axis=0)]) - x
+		z = np.dot(P, img_test)
+		tmp = np.zeros(np.size(Y, axis=1))
+		for k in range(np.size(Y, axis=1)):
+			tmp[k] = np.linalg.norm(Y[:, k] - z)
+		value = np.min(tmp)
+		value_index = np.argwhere(tmp == value)[0][0]
+		if value < THRESHOLD:
+			label[count] = value_index
+		else:
+			label[count] = -1
+		count += 1
+print(label)
+accuracy = 0
+cut = 0
+for i in range(len(data)):
+	cut_out = cut + np.size(data[i], axis=2)
+	for j in range(cut, cut_out):
+		if label[j] >= cut and label[j] < cut_out:
+			accuracy += 1
+	cut = cut_out
+accuracy = accuracy / len(label)
+print(accuracy)
