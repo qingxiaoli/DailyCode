7117e2e8dc4d9a2daa70c4c4cfe80eb489b67905
Jason An <pkuanjie@gmail.com>
2017-04-10 20:28:31 +0800

    finish hw2 of cv

--- matlab/computer_vision/hw2/ransac_test.m
+++ matlab/computer_vision/hw2/ransac_test.m
@@ -30,9 +30,15 @@ tmp = [A, ones(size(A, 1), 1)] * X;
 B = tmp(:, 1: 2) ./ repmat(tmp(:, 3), 1, 2);
 figure,
 plot(A(:, 1), A(:, 2), '.');
+title('original A scatter');
 figure,
 plot(B(:, 1), B(:, 2), '.');
+title('original B scatter');
 [best_model, best_consensus_set_A, best_consensus_set_B, best_error] = ransac(A, B, N, K, T, D);
-B_pre = [A, ones(size(A, 1), 1)] * best_model;
-figure, plot(B_pre(:, 1), B_pre(:, 2), '.');
+tmp = [A, ones(size(A, 1), 1)] * best_model;
+B_pre = [A, ones(size(A, 1), 1)] * best_model ./ repmat(tmp(:, 3), [1, 3]);
+figure, 
+plot(B_pre(:, 1), B_pre(:, 2), '.');
+title('predict B scatter');
+
 
--- matlab/computer_vision/hw2/two_image_mosaic_test.m
+++ matlab/computer_vision/hw2/two_image_mosaic_test.m
@@ -6,17 +6,46 @@
 %% import images
 PATH1 = 'uttower_left.jpg';
 PATH2 = 'uttower_right.jpg';
+img1 = imread(PATH1);
+img2 = imread(PATH2);
 
-img1 = rgb2gray(imread(PATH1));
-img2 = rgb2gray(imread(PATH2));
-
-mean1 = mean(mean(img1));
-mean2 = mean(mean(img2));
+mean1 = mean(mean(double(rgb2gray(img1))));
+mean2 = mean(mean(double(rgb2gray(img2))));
 
 m = mean([mean1, mean2]);
 
-img1 = single(img1 * (m / mean1));
-img2 = single(img2 * (m / mean2));
+for i = 1 : 3
+    img1(:, :, i) = single(img1(:, :, i) * (m / mean1));
+    img2(:, :, i) = single(img2(:, :, i) * (m / mean2));
+end;
 
 %% mosaic
-mosaic1 = two_image_mosaic(img1, img2);
+mosaic = two_image_mosaic(img1, img2);
+figure, 
+imshow(mosaic),
+title('mosaic result');
+
+%% old and new mosaic test failed
+% %% import images
+% PATH1 = 'spring_left.JPG';
+% PATH2 = 'winter_middle.jpg';
+% img1 = imread(PATH1);
+% img2 = imread(PATH2);
+% img1 = imresize(img1, [400, 600]);
+% img2 = imresize(img2, [400, 600]);
+% 
+% mean1 = mean(mean(double(rgb2gray(img1))));
+% mean2 = mean(mean(double(rgb2gray(img2))));
+% 
+% m = mean([mean1, mean2]);
+% 
+% for i = 1 : 3
+%     img1(:, :, i) = single(img1(:, :, i) * (m / mean1));
+%     img2(:, :, i) = single(img2(:, :, i) * (m / mean2));
+% end;
+% 
+% %% mosaic
+% mosaic = two_image_mosaic(img1, img2);
+% figure, 
+% imshow(mosaic),
+% title('mosaic result');
